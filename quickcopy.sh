03ce59b197ba20e357d64f7df832b97045a0cc16
Dedzsinator <degi.nandor@gmail.com>
2023-08-28 01:48:05 +0300

    uploaded neovim config

---
+++ quickcopy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Specify the source files and the destination directory
+file1="../.zshrc"
+file2="../.p10k.zsh"
+folder="../.config"
+destination_dir="./"
+
+# Copy the files to the destination directory
+sudo cp "$file1" "$destination_dir"
+sudo cp "$file2" "$destination_dir"
+sudo cp -r "$folder" "$destination_dir"
+
+# Print a message indicating successful copy
+echo "Files copied to $destination_dir"
